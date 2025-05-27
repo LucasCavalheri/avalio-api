61c722188729bd42127292198aa1948a699ad5e2
Lucas Cavalheri <lucas.dev.carvalho@gmail.com>
2025-05-27 20:47:22 -0300

    usa retorno padrão da api

--- app/Http/Controllers/Stripe/SubscriptionHistoryController.php
+++ app/Http/Controllers/Stripe/SubscriptionHistoryController.php
@@ -17,19 +17,16 @@ class SubscriptionHistoryController extends Controller
         $user = $request->user();
 
         try {
-            // Obtém a assinatura atual
             $subscription = $user->subscription('default');
 
-            // Adiciona a data da próxima cobrança se a assinatura estiver ativa
             if ($subscription && $subscription->stripe_status === 'active' && ! $subscription->canceled()) {
                 $stripeSubscription = $subscription->asStripeSubscription();
                 $subscription->next_payment = $stripeSubscription->current_period_end;
             }
 
-            // Obtém o histórico de faturas/pagamentos
             $invoices = $user->invoices();
 
-            return response()->json([
+            return $this->success('Histórico obtido com sucesso', Response::HTTP_OK, [
                 'subscription' => $subscription ? SubscriptionHistoryResource::make($subscription) : null,
                 'invoices' => $invoices->map(function ($invoice) {
                     return [
@@ -42,12 +39,7 @@ class SubscriptionHistoryController extends Controller
                 }),
             ]);
         } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Erro ao obter histórico',
-                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
-                'errors' => [$e->getMessage()],
-                'data' => [],
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->error('Erro ao obter histório', Response::HTTP_INTERNAL_SERVER_ERROR, [$e->getMessage()]);
         }
     }
 }
